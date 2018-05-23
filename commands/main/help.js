1178d1bb2db991071b707c8ae949d2f6665f5585
Paul52Games <31313717+Paul52Games@users.noreply.github.com>
2018-05-23 20:02:57 +0200

    bug fix bij help

--- commands/main/help.js
+++ commands/main/help.js
@@ -1,9 +1,16 @@
 const Discord = require("discord.js");
 exports.run = async (client, message, args, data, config, queue) => {
+  if (args[0] == "test") {
+    await client.category.forEach(category => {
+      await category.forEach(command => {
+        console.log(`${category} => ${command.name.}`);
+      })
+    })
+    await return;
+  }
   if (!args[0]) {
     let prefix = data.get(`${message.guild.id}.prefix`)
     let embed = new Discord.RichEmbed()
-    // .setDescription(`**<>** Verplicht\n**[]** Niet verplict\n\n**${prefix}help**\n*Open dit helpmenu*\n**${prefix}shop <sub categorie> <voorwerp> [aantal]**\n*Koop iets*\n**${prefix}inv [gebruiker]**\n*Open jouw of iemand anders inventory*\n**${prefix}level**\n*Bekijk jouw level en XP*\n**${prefix}open <pack> [aantal]**\n*Open een pack voor rewards*\n**${prefix}pay <gebruiker> <aantal>**\n*Geef een gebruiker een bepaald aantal geld*\n**${prefix}trade <gebruiker> <pack> [aantal]**\n*Geef een gebruiker een bepaald aantal packs*\n**${prefix}beveiliging <niveau> <tijd in uren>**\n*Koop een beveiliging tegen de dieven*`)
     .setTitle("Help")
     .setColor("#00ff00")
     let cmds = '';
