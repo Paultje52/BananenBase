e1145bbda16d873d73b620c38930a39e6bd6b3fd
Paul52Games <31313717+Paul52Games@users.noreply.github.com>
2018-06-09 19:01:25 +0200

    Bug fix

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 exports.run = (client, ...args) => {
-  console.log(`Ik ben online in ${Date.now() - client.start}ms! Ik heet ${client.user.username} en ik zit in ${client.guilds.size} servers bij ${client.users.size - 1} members in ${client.channels.size} channels!`);
+  console.log(`\nIk ben online in ${Date.now() - client.start}ms! Ik heet ${client.user.username} en ik zit in ${client.guilds.size} servers bij ${client.users.size - 1} members in ${client.channels.size} channels!`);
 }
 exports.config = {
   enable: true
