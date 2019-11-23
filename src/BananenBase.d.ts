b86087cb959c3f2aaace2241a2e3c3e0e8064aac
770grappenmaker <50543283+770grappenmaker@users.noreply.github.com>
2019-11-23 10:37:48 +0100

    Update exports

--- src/BananenBase.d.ts
+++ src/BananenBase.d.ts
@@ -1,81 +1,6 @@
-declare module "bananenbase" {
-	export class BananenBase {
-		constructor(options: BananenBaseOptions)
-	}
-	export class command {
-		constructor(
-			client: object,
-			help: {
-				name: string,
-				description?: string,
-				category?: string,
-				subCommands?: [string],
-				enabled?: boolean,
-				guildOnly?: boolean,
-				args?: [string]
-			},
-			checking?: {
-				permLevel: number,
-				permissions?: {
-					me: [string],
-					user: [string]
-				},
-				func?: object
-			}
-		)
-		run(
-			message: object,
-			args?: [string | undefined],
-			client?: object
-		): object
-		prepare(
-			message: object,
-			args?: [string | undefined]
-		): object
-		done(
-			message: object,
-			args?: [string | undefined]
-		): object
-	}
-	interface BananenBaseOptions {
-		token: string,
-		database: {
-			package: "keyv" | "json";
-			name: string;
-			type?: "mongodb" | "redis" | "sqlite" | "postgres" | "mysql";
-			code?: string;
-		},
-		language: "EN" | "NL",
-		bot?: object,
-		settings?: boolean,
-		prefix: string,
-		botConfig?: {
-			botOwners: [string],
-			authorSettings: object,
-			guildSettings: {
-				prefix: string,
-				embed: {
-					color: string,
-					footerText: string,
-					time: boolean,
-					footerImage: string
-				}
-			}
-		},
-		ignore?: {
-			bot: boolean,
-			pm: boolean
-		},
-		server?: string | object,
-		webPort?: number,
-		consoleFunctions?: boolean,
-		permissionLevels?: [object],
-		pmPrefix: boolean,
-		commandErrorThrowing?: boolean,
-		eventEmitterMaxFuncions?: number
-	}
-}
-export = class BananenBase {
+import * as discord from 'discord.js';
+
+declare class BananenBase {
 	constructor(options: BananenBaseOptions)
 }
 interface BananenBaseOptions {
@@ -115,3 +40,56 @@ interface BananenBaseOptions {
 	commandErrorThrowing?: boolean,
 	eventEmitterMaxFuncions?: number
 }
+
+declare class BananenBase_Command {
+	constructor(
+		client: discord.Client,
+		help: {
+			name: string,
+			description?: string,
+			category?: string,
+			subCommands?: [string],
+			enabled?: boolean,
+			guildOnly?: boolean,
+			args?: [string]
+		},
+		checking?: {
+			permLevel: number,
+			permissions?: {
+				me: [string],
+				user: [string]
+			},
+			func?: object
+		}
+	)
+	run(
+		message: object,
+		args?: [string | undefined],
+		client?: object
+	): object
+	prepare(
+		message: object,
+		args?: [string | undefined]
+	): object
+	done(
+		message: object,
+		args?: [string | undefined]
+	): object
+	client: discord.Client
+}
+
+declare class BananenBase_Event {
+	constructor(
+		client: discord.Client,
+		help: {
+			name: string,
+		}
+	)
+	run(
+		event: any
+	): object
+	client: discord.Client
+}
+export = BananenBase;
+export const command = BananenBase_Command;
+export const event = BananenBase_Event;
