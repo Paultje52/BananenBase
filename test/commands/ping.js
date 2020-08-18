65dc682d21f303fe0aa057bb4d99934eb1d96af0
Paultje52 <paultje52@gmail.com>
2020-08-18 14:11:40 +0200

    Fixed ping command

--- test/commands/ping.js
+++ test/commands/ping.js
@@ -26,14 +26,14 @@ module.exports = class Command extends BananenBase.command {
   async run(message, _args) {
     let start = Date.now();
     let msg = await message.channel.send("Pinging...");
-    let ping = Date.now()-start-this.client.ping;
+    let ping = Date.now()-start-this.client.ws.ping;
 
     if (message.flags.includes("avarage")) {
       await msg.edit("1");
       await msg.edit("2");
-      ping = (Date.now()-start-this.client.ping)/3;
+      ping = (Date.now()-start-this.client.ws.ping)/3;
     }
 
-    msg.edit(`:ping_pong: ${Math.floor(ping)}ms\n:blue_heart: ${Math.floor(this.client.ping)}ms`);
+    msg.edit(`:ping_pong: ${Math.floor(ping)}ms\n:blue_heart: ${Math.floor(this.client.ws.ping)}ms`);
   }
 }
