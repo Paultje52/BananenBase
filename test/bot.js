cd10e1d12e09d67a7d11bd9787f5b79b8368a487
Paultje52 <paultje52@gmail.com>
2020-08-18 13:21:03 +0200

    Made example cleaner

--- test/bot.js
+++ test/bot.js
@@ -1,20 +1,24 @@
 const BananenBase = require("../src/BananenBase");
 
-new BananenBase("TOKEN")
-  .setConfig(require("./config.json"))
-  .addModule(["loader", "alias", "database", "messageflags", "args"])
-  .addModule("security", {
-    defaultPermissions: {
-      user: ["SEND_MESSAGES"],
-      bot: ["EMBED_LINKS"]
-    },
-    permissionNames: {
-      botOwner: (message, BananenBase) => {
-        let botOwners = ["327462385361092621", "259776081316282368"];
-        return botOwners.includes(message.author.id);
-      }
+let bot = new BananenBase("TOKEN");
+
+bot.setConfig(require("./config.json"));
+bot.addModule(["loader", "alias", "database", "messageflags", "args"]);
+bot.addModule("security", {
+  defaultPermissions: {
+    user: ["SEND_MESSAGES"],
+    bot: ["EMBED_LINKS"]
+  },
+  permissionNames: {
+    botOwner: (message, BananenBase) => {
+      let botOwners = ["327462385361092621", "259776081316282368"];
+      return botOwners.includes(message.author.id);
     }
-  })
-  .ready((BananenBase) => {
-    BananenBase.start();
-  });
+  }
+})
+
+bot.ready((BananenBase) => {
+  BananenBase.start();
+}).then(() => {
+  console.log("Starting bot!");
+});
